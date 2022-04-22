4e870c3d376bbb10bd40e64be6205e6f8281ff2f
Dell Precision <mfyfeatlethenty@hotmail.co.uk>
2022-04-22 18:45:47 +0100

    First Commit

---
+++ Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Merit.IdentityServer.API.Pages.Account;
+
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,8 @@
+
+namespace Merit.IdentityServer.API.Pages.Logout;
+
+public class LogoutOptions
+{
+    public static bool ShowLogoutPrompt = true;
+    public static bool AutomaticRedirectAfterSignOut = false;
+}
